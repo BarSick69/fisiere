5a9b6c2afe855208059d6863fc3b9c4c41a69da4
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2021-01-13 22:36:10 +0200

    Add files via upload

---
+++ pr2.py
@@ -0,0 +1,18 @@
+
+with open('numere.txt', 'r') as f:
+    a = f.readline()
+    b = f.readline()
+if int(a) > int(b):
+    a = int(a) * 2
+    b = int(b) * 3
+elif int(b) > int(a):
+    a = int(a) * 3
+    b = int(b) * 2
+else:
+    a = b = 'Numerele sunt egale'
+if a != b:
+    with open('produs.txt', 'w') as f:
+        f.write(f'{a}\n{b}')
+else:
+    with open('produs.txt', 'w') as f:
+        f.write(f'{a}')
