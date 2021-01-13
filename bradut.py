91f076c59718171854c862ff83a01a4acad03b08
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2021-01-13 20:24:32 +0200

    Delete bradut.py

--- bradut.py
+++
@@ -1,9 +0,0 @@
-with open("globulet.txt",'r')as f:
-    a=f.readline()
-b=int(a)+3
-c=int(a)+b-3
-with open("bradut.txt",'w')as f:
-    a=f.writelines(str(int(a)+b+c))
-    a=f.writelines("\n")
-    a=f.writelines("da")
-
